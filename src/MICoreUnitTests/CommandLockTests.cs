7230ca1ccc4ddb8027483fe237811c3c025948e5
Chuck Ries <chuckr@microsoft.com>
2015-06-05 17:52:25 -0700

    Move remaing MICoreUnitTests to xunit.

--- src/MICoreUnitTests/CommandLockTests.cs
+++ src/MICoreUnitTests/CommandLockTests.cs
@@ -2,146 +2,145 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using MICore;
+using Xunit;
 
 namespace MICoreUnitTests
 {
-    [TestClass]
     public class CommandLockTests
     {
-        [TestMethod]
+        [Fact]
         public void ExclusiveTest1()
         {
             using (var commandLock = new CommandLock())
             {
                 Task<ExclusiveLockToken> t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 Task<ExclusiveLockToken> t2 = commandLock.AquireExclusive();
-                Assert.IsFalse(t2.IsCompleted);
+                Assert.False(t2.IsCompleted);
 
                 ExclusiveLockToken token = t1.Result;
                 token.Close();
-                Assert.IsTrue(t2.IsCompleted, "Closing t1 should signal t2");
-                Assert.IsTrue(ExclusiveLockToken.IsNullOrClosed(token), "Closing the token should zero it out");
+                Assert.True(t2.IsCompleted, "Closing t1 should signal t2");
+                Assert.True(ExclusiveLockToken.IsNullOrClosed(token), "Closing the token should zero it out");
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void SharedTest1()
         {
             using (var commandLock = new CommandLock())
             {
                 // should be able to aquire multiple shared locks
                 Task t1 = commandLock.AquireShared();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 Task t2 = commandLock.AquireShared();
-                Assert.IsTrue(t2.IsCompleted);
+                Assert.True(t2.IsCompleted);
 
                 Task<ExclusiveLockToken> t3 = commandLock.AquireExclusive();
-                Assert.IsFalse(t3.IsCompleted);
+                Assert.False(t3.IsCompleted);
 
                 commandLock.ReleaseShared();
-                Assert.IsFalse(t3.IsCompleted);
+                Assert.False(t3.IsCompleted);
 
                 commandLock.ReleaseShared();
-                Assert.IsTrue(t3.IsCompleted);
+                Assert.True(t3.IsCompleted);
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void ExclusiveThenSharedTest()
         {
             using (var commandLock = new CommandLock())
             {
                 // Part 1 - try and aquire the shared lock while the exclusive lock is held
                 Task<ExclusiveLockToken> t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 Task t2 = commandLock.AquireShared();
-                Assert.IsFalse(t2.IsCompleted);
+                Assert.False(t2.IsCompleted);
 
                 ExclusiveLockToken token = t1.Result;
                 token.Close();
-                Assert.IsTrue(t2.IsCompleted, "Closing t1 should signal t2");
-                Assert.IsTrue(ExclusiveLockToken.IsNullOrClosed(token), "Closing the token should zero it out");
+                Assert.True(t2.IsCompleted, "Closing t1 should signal t2");
+                Assert.True(ExclusiveLockToken.IsNullOrClosed(token), "Closing the token should zero it out");
 
                 commandLock.ReleaseShared();
 
 
                 // Part 2 - release the exclusive lock before attempting to aquire the shared lock
                 t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 t1.Result.Close();
 
                 t2 = commandLock.AquireShared();
-                Assert.IsTrue(t2.IsCompleted, "Shared lock should be immediately aquired");
+                Assert.True(t2.IsCompleted, "Shared lock should be immediately aquired");
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void SharedThenExclusiveTest()
         {
             using (var commandLock = new CommandLock())
             {
                 // should be able to aquire multiple shared locks
                 Task t1 = commandLock.AquireShared();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 Task t2 = commandLock.AquireShared();
-                Assert.IsTrue(t2.IsCompleted);
+                Assert.True(t2.IsCompleted);
 
                 commandLock.ReleaseShared();
                 commandLock.ReleaseShared();
 
                 Task<ExclusiveLockToken> t3 = commandLock.AquireExclusive();
-                Assert.IsTrue(t3.IsCompleted);
+                Assert.True(t3.IsCompleted);
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void ConvertToSharedLockTest1()
         {
             // NOTE: This test covers the case that there ARE pending shared locks
             using (var commandLock = new CommandLock())
             {
                 Task<ExclusiveLockToken> t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 Task t2 = commandLock.AquireShared();
-                Assert.IsFalse(t2.IsCompleted);
+                Assert.False(t2.IsCompleted);
 
                 ExclusiveLockToken token = t1.Result;
                 token.ConvertToSharedLock();
-                Assert.IsTrue(ExclusiveLockToken.IsNullOrClosed(token), "ConvertToSharedLock should have nulled out the token");
+                Assert.True(ExclusiveLockToken.IsNullOrClosed(token), "ConvertToSharedLock should have nulled out the token");
 
-                Assert.IsTrue(t2.IsCompleted);
+                Assert.True(t2.IsCompleted);
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void ConvertToSharedLockTest2()
         {
             // NOTE: This test covers the case that there are NOT pending shared locks
             using (var commandLock = new CommandLock())
             {
                 Task<ExclusiveLockToken> t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 ExclusiveLockToken token = t1.Result;
                 token.ConvertToSharedLock();
-                Assert.IsTrue(ExclusiveLockToken.IsNullOrClosed(token), "ConvertToSharedLock should have nulled out the token");
+                Assert.True(ExclusiveLockToken.IsNullOrClosed(token), "ConvertToSharedLock should have nulled out the token");
 
                 Task t2 = commandLock.AquireShared();
-                Assert.IsTrue(t2.IsCompleted);
+                Assert.True(t2.IsCompleted);
             }
         }
 
-        [TestMethod]
+        [Fact]
         public void CloseAbortsOperationsTest()
         {
             Task[] pendingTasks = new Task[2];
@@ -149,21 +148,21 @@ namespace MICoreUnitTests
             using (var commandLock = new CommandLock())
             {
                 Task<ExclusiveLockToken> t1 = commandLock.AquireExclusive();
-                Assert.IsTrue(t1.IsCompleted);
+                Assert.True(t1.IsCompleted);
 
                 pendingTasks[0] = commandLock.AquireExclusive();
-                Assert.IsFalse(pendingTasks[0].IsCompleted);
+                Assert.False(pendingTasks[0].IsCompleted);
 
                 pendingTasks[1] = commandLock.AquireShared();
-                Assert.IsFalse(pendingTasks[1].IsCompleted);
+                Assert.False(pendingTasks[1].IsCompleted);
 
                 commandLock.Close();
             }
 
             foreach (Task t in pendingTasks)
             {
-                Assert.IsTrue(t.IsCompleted);
-                Assert.IsInstanceOfType(t.Exception.InnerException, typeof(ObjectDisposedException));
+                Assert.True(t.IsCompleted);
+                Assert.IsType(typeof(ObjectDisposedException), t.Exception.InnerException);
             }
         }
     }
--- src/MICoreUnitTests/NdkVersionTests.cs
+++ src/MICoreUnitTests/NdkVersionTests.cs
@@ -2,83 +2,79 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using AndroidDebugLauncher;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace MICoreUnitTests
 {
-    [TestClass]
     public class NdkVersionTests
     {
-        [TestMethod]
+        [Fact]
         public void TestNdkToolVersion1()
         {
             NdkToolVersion v;
-            Assert.IsTrue(NdkToolVersion.TryParse("1.2.3.4.5", out v));
-            Assert.AreEqual(v.ToString(), "1.2.3.4.5");
+            Assert.True(NdkToolVersion.TryParse("1.2.3.4.5", out v));
+            Assert.Equal(v.ToString(), "1.2.3.4.5");
 
-            Assert.IsTrue(NdkToolVersion.TryParse("6.7", out v));
-            Assert.AreEqual(v.ToString(), "6.7");
+            Assert.True(NdkToolVersion.TryParse("6.7", out v));
+            Assert.Equal(v.ToString(), "6.7");
 
-            Assert.IsFalse(NdkToolVersion.TryParse("1", out v));
-            Assert.IsFalse(NdkToolVersion.TryParse("1.0-beta", out v));
-            Assert.IsFalse(NdkToolVersion.TryParse("No.Version", out v));
+            Assert.False(NdkToolVersion.TryParse("1", out v));
+            Assert.False(NdkToolVersion.TryParse("1.0-beta", out v));
+            Assert.False(NdkToolVersion.TryParse("No.Version", out v));
         }
 
-        [TestMethod]
+        [Fact]
         public void TestNdkToolVersion2()
         {
             NdkToolVersion v1, v2, v3;
-            Assert.IsTrue(NdkToolVersion.TryParse("1.2.3", out v1));
-            Assert.IsTrue(NdkToolVersion.TryParse("1.2", out v2));
-            Assert.IsTrue(NdkToolVersion.TryParse("2.0", out v3));
+            Assert.True(NdkToolVersion.TryParse("1.2.3", out v1));
+            Assert.True(NdkToolVersion.TryParse("1.2", out v2));
+            Assert.True(NdkToolVersion.TryParse("2.0", out v3));
 
-            Assert.IsTrue(v1.CompareTo(v2) > 0);
-            Assert.IsTrue(v2.CompareTo(v1) < 0);
+            Assert.True(v1.CompareTo(v2) > 0);
+            Assert.True(v2.CompareTo(v1) < 0);
 
-            Assert.IsTrue(v2.CompareTo(v3) < 0);
-            Assert.IsTrue(v3.CompareTo(v2) > 0);
+            Assert.True(v2.CompareTo(v3) < 0);
+            Assert.True(v3.CompareTo(v2) > 0);
 
-            Assert.IsTrue(v1.CompareTo(new NdkToolVersion()) > 0);
+            Assert.True(v1.CompareTo(new NdkToolVersion()) > 0);
 
-            Assert.IsTrue(v1.CompareTo(v1) == 0);
-            Assert.IsTrue(v2.CompareTo(v2) == 0);
-            Assert.IsTrue(v3.CompareTo(v3) == 0);
+            Assert.True(v1.CompareTo(v1) == 0);
+            Assert.True(v2.CompareTo(v2) == 0);
+            Assert.True(v3.CompareTo(v3) == 0);
 
             NdkToolVersion[] versions = { v1, v2, v3 };
             IEnumerable<string> versionStrings = versions.OrderByDescending((x) => x).Select((x) => x.ToString());
             string orderedResults = string.Join(", ", versionStrings);
-            Assert.AreEqual(orderedResults, "2.0, 1.2.3, 1.2");
+            Assert.Equal(orderedResults, "2.0, 1.2.3, 1.2");
         }
 
-        [TestMethod]
+        [Fact]
         public void TestNdkReleaseId1()
         {
             NdkReleaseId r;
-            Assert.IsTrue(NdkReleaseId.TryParse("r1", out r));
-            Assert.AreEqual(r.ToString(), "r1");
+            Assert.True(NdkReleaseId.TryParse("r1", out r));
+            Assert.Equal(r.ToString(), "r1");
 
-            Assert.IsTrue(NdkReleaseId.TryParse("r1a", out r));
-            Assert.AreEqual(r.ToString(), "r1a");
+            Assert.True(NdkReleaseId.TryParse("r1a", out r));
+            Assert.Equal(r.ToString(), "r1a");
 
-            Assert.IsTrue(NdkReleaseId.TryParse("r10", out r));
-            Assert.AreEqual(r.ToString(), "r10");
+            Assert.True(NdkReleaseId.TryParse("r10", out r));
+            Assert.Equal(r.ToString(), "r10");
 
-            Assert.IsTrue(NdkReleaseId.TryParse("r10b (64-bit)", out r));
-            Assert.AreEqual(r.ToString(), "r10b (64-bit)");
+            Assert.True(NdkReleaseId.TryParse("r10b (64-bit)", out r));
+            Assert.Equal(r.ToString(), "r10b (64-bit)");
 
-            Assert.IsFalse(NdkReleaseId.TryParse("100", out r));
-            Assert.IsFalse(NdkReleaseId.TryParse("r", out r));
-            Assert.IsFalse(NdkReleaseId.TryParse("ra", out r));
-            Assert.IsFalse(NdkReleaseId.TryParse("r10 jam", out r));
-            Assert.IsFalse(NdkReleaseId.TryParse("r10!", out r));
+            Assert.False(NdkReleaseId.TryParse("100", out r));
+            Assert.False(NdkReleaseId.TryParse("r", out r));
+            Assert.False(NdkReleaseId.TryParse("ra", out r));
+            Assert.False(NdkReleaseId.TryParse("r10 jam", out r));
+            Assert.False(NdkReleaseId.TryParse("r10!", out r));
         }
 
-        [TestMethod]
+        [Fact]
         public void TestNdkReleaseId2()
         {
             NdkReleaseId r10 = new NdkReleaseId(10, (char)0, true);
@@ -86,12 +82,12 @@ namespace MICoreUnitTests
             NdkReleaseId r10b = new NdkReleaseId(10, 'b', true);
             NdkReleaseId r9d = new NdkReleaseId(9, 'd', true);
 
-            Assert.IsTrue(r10.CompareVersion(r10) == 0);
-            Assert.IsTrue(r10.CompareVersion(r10_64bit) == 0);
-            Assert.IsTrue(r10.CompareVersion(r10b) < 0);
-            Assert.IsTrue(r10b.CompareVersion(r10) > 0);
-            Assert.IsTrue(r10.CompareVersion(r9d) > 0);
-            Assert.IsTrue(r9d.CompareVersion(r10) < 0);
+            Assert.True(r10.CompareVersion(r10) == 0);
+            Assert.True(r10.CompareVersion(r10_64bit) == 0);
+            Assert.True(r10.CompareVersion(r10b) < 0);
+            Assert.True(r10b.CompareVersion(r10) > 0);
+            Assert.True(r10.CompareVersion(r9d) > 0);
+            Assert.True(r9d.CompareVersion(r10) < 0);
         }
     }
 }
