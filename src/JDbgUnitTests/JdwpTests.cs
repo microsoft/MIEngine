782634853a22cbdddb94f74c27702b9b4b7d8b7f
Chuck Ries <chuckr@microsoft.com>
2015-06-08 10:45:21 -0700

    Move JDbg unit tests to xunit

--- src/JDbgUnitTests/JdwpTests.cs
+++ src/JDbgUnitTests/JdwpTests.cs
@@ -2,32 +2,31 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JDbg;
+using Xunit;
 
 namespace JDbgUnitTests
 {
-    [TestClass]
     public class JdwpTests
     {
-        [TestMethod]
+        [Fact]
         public void VersionPacketTest()
         {
             var versionCommand = new VersionCommand();
 
             byte[] packetBytes = versionCommand.GetPacketBytes();
 
-            Assert.AreEqual(0x00, packetBytes[0]);
-            Assert.AreEqual(0x00, packetBytes[1]);
-            Assert.AreEqual(0x00, packetBytes[2]);
-            Assert.AreEqual(0x0b, packetBytes[3]);
-            Assert.AreEqual(0x00, packetBytes[4]);
-            Assert.AreEqual(0x00, packetBytes[5]);
-            Assert.AreEqual(0x00, packetBytes[6]);
-            Assert.AreEqual(0x01, packetBytes[7]);
-            Assert.AreEqual(0x00, packetBytes[8]);
-            Assert.AreEqual(0x01, packetBytes[9]);
-            Assert.AreEqual(0x01, packetBytes[10]);
+            Assert.Equal(0x00, packetBytes[0]);
+            Assert.Equal(0x00, packetBytes[1]);
+            Assert.Equal(0x00, packetBytes[2]);
+            Assert.Equal(0x0b, packetBytes[3]);
+            Assert.Equal(0x00, packetBytes[4]);
+            Assert.Equal(0x00, packetBytes[5]);
+            Assert.Equal(0x00, packetBytes[6]);
+            Assert.Equal(0x01, packetBytes[7]);
+            Assert.Equal(0x00, packetBytes[8]);
+            Assert.Equal(0x01, packetBytes[9]);
+            Assert.Equal(0x01, packetBytes[10]);
         }
     }
 }
