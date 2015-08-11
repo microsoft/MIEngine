e0f344d5800b2fae37a0d2022cf03c6be6e9208f
Pawel Hofman <pawel@codetitans.pl>
2015-08-11 03:52:48 +0200

    Added BlackBerry specific launcher
    Basic debugging works, although some bugs occure.

---
+++ src/BlackBerryDebugLauncher/BlackBerryLaunchOptions.cs
@@ -0,0 +1,84 @@
+﻿using MICore;
+using System;
+
+namespace BlackBerryDebugLauncher
+{
+    internal sealed class BlackBerryLaunchOptions
+    {
+        public BlackBerryLaunchOptions(string exePath, MICore.Xml.LaunchOptions.BlackBerryLaunchOptions xmlOptions)
+        {
+            if (string.IsNullOrEmpty(exePath))
+                throw new ArgumentNullException("exePath");
+            if (xmlOptions == null)
+                throw new ArgumentNullException("xmlOptions");
+
+            GdbPath = LaunchOptions.RequireAttribute(xmlOptions.GdbPath, "GdbPath");
+            GdbHostPath = LaunchOptions.RequireAttribute(xmlOptions.GdbHostPath, "GdbHostPath");
+            PID = xmlOptions.PID;
+            ExePath = exePath;
+            TargetAddress = LaunchOptions.RequireAttribute(xmlOptions.TargetAddress, "TargetAddress");
+            TargetPort = xmlOptions.TargetPort;
+            IsAttach = xmlOptions.Attach;
+            AdditionalSOLibSearchPath = xmlOptions.AdditionalSOLibSearchPath;
+            TargetArchitecture = LaunchOptions.ConvertTargetArchitectureAttribute(xmlOptions.TargetArchitecture);
+        }
+
+        #region Properties
+
+        public string GdbPath
+        {
+            get;
+            private set;
+        }
+
+        public string GdbHostPath
+        {
+            get;
+            private set;
+        }
+
+        public string TargetAddress
+        {
+            get;
+            private set;
+        }
+
+        public uint TargetPort
+        {
+            get;
+            private set;
+        }
+
+        public TargetArchitecture TargetArchitecture
+        {
+            get;
+            private set;
+        }
+
+        public uint PID
+        {
+            get;
+            private set;
+        }
+
+        public string ExePath
+        {
+            get;
+            private set;
+        }
+
+        public bool IsAttach
+        {
+            get;
+            private set;
+        }
+
+        public string AdditionalSOLibSearchPath
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+    }
+}
