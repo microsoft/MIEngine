61e596ffd71a8a4a86c9567fd992495b82d2adc4
Gregg Miskelly <greggm@microsoft.com>
2016-02-04 14:29:46 -0800

    Add telemetry for debugger abort
    This checkin updates MIEngine to fire telemetry when the underlying debugger exits unexpectedly.

--- src/MIDebugEngine/Engine.Impl/DebuggedProcess.cs
+++ src/MIDebugEngine/Engine.Impl/DebuggedProcess.cs
@@ -43,6 +43,7 @@ namespace Microsoft.MIDebugEngine
         public readonly Natvis.Natvis Natvis;
         private ReadOnlyCollection<RegisterDescription> _registers;
         private ReadOnlyCollection<RegisterGroup> _registerGroups;
+        private readonly EngineTelemetry _engineTelemetry = new EngineTelemetry();
 
         public DebuggedProcess(bool bLaunched, LaunchOptions launchOptions, ISampleEngineCallback callback, WorkerThread worker, BreakpointManager bpman, AD7Engine engine, HostConfigurationStore configStore) : base(launchOptions)
         {
@@ -258,6 +259,8 @@ namespace Microsoft.MIDebugEngine
                 // The MI debugger process unexpectedly exited.
                 _worker.PostOperation(() =>
                     {
+                        _engineTelemetry.SendDebuggerAborted(MICommandFactory, GetLastSentCommandName(), debuggerExitCode);
+
                         // If the MI Debugger exits before we get a resume call, we have no way of sending program destroy. So just let start debugging fail.
                         if (!_connected)
                         {
@@ -396,7 +399,7 @@ namespace Microsoft.MIDebugEngine
             {
                 string eventName;
                 KeyValuePair<string, object>[] properties;
-                if (DecodeTelemetryEvent(args.Results, out eventName, out properties))
+                if (_engineTelemetry.DecodeTelemetryEvent(args.Results, out eventName, out properties))
                 {
                     HostTelemetry.SendEvent(eventName, properties);
                 }
@@ -1166,57 +1169,6 @@ namespace Microsoft.MIDebugEngine
 
             uint errorCode = results.TryFindUint("error-code") ?? 0;
             return new OutputMessage(text, messageType, severity, errorCode);
-        }
-
-        private bool DecodeTelemetryEvent(Results results, out string eventName, out KeyValuePair<string, object>[] properties)
-        {
-            properties = null;
-
-            // NOTE: the message event is an MI Extension from clrdbg, though we could use in it the future for other debuggers
-            eventName = results.TryFindString("event-name");
-            if (string.IsNullOrEmpty(eventName) || !char.IsLetter(eventName[0]) || !eventName.Contains('/'))
-            {
-                Debug.Fail("Bogus telemetry event. 'Event-name' property is missing or invalid.");
-                return false;
-            }
-
-            TupleValue tuple;
-            if (!results.TryFind("properties", out tuple))
-            {
-                Debug.Fail("Bogus message event, missing 'properties' property");
-                return false;
-            }
-
-            List<KeyValuePair<string, object>> propertyList = new List<KeyValuePair<string, object>>(tuple.Content.Count);
-            foreach (NamedResultValue pair in tuple.Content)
-            {
-                ConstValue resultValue = pair.Value as ConstValue;
-                if (resultValue == null)
-                    continue;
-
-                string content = resultValue.Content;
-                if (string.IsNullOrEmpty(content))
-                    continue;
-
-                object value = content;
-                int numericValue;
-                if (content.Length >= 3 && content.StartsWith("0x", StringComparison.OrdinalIgnoreCase) && int.TryParse(content.Substring(2), NumberStyles.AllowHexSpecifier, CultureInfo.InvariantCulture, out numericValue))
-                {
-                    value = numericValue;
-                }
-                else if (int.TryParse(content, NumberStyles.None, CultureInfo.InvariantCulture, out numericValue))
-                {
-                    value = numericValue;
-                }
-
-                if (value != null)
-                {
-                    propertyList.Add(new KeyValuePair<string, object>(pair.Name, value));
-                }
-            }
-
-            properties = propertyList.ToArray();
-            return true;
         }
 
         private static RegisterGroup GetGroupForRegister(List<RegisterGroup> registerGroups, string name, EngineUtils.RegisterNameMap nameMap)
