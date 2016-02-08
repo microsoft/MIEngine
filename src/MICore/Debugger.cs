cbaccd842170954fb1844ed1209d3b717e6d3264
Wiktor Kopec <wiktork@microsoft.com>
2016-02-08 11:14:40 -0800

    Adding platform check for CmdLinuxBreak

--- src/MICore/Debugger.cs
+++ src/MICore/Debugger.cs
@@ -485,7 +485,8 @@ namespace MICore
 
         public Task CmdBreakInternal()
         {
-            if (IsLocalGdbAttach())
+            //TODO May need to fix attach on windows and osx.
+            if (IsLocalGdbAttach() && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
                 // for local linux debugging with attach, send a signal to one of the debugee processes rather than 
                 // using -exec-interrupt. -exec-interrupt does not work with attach. End result is either
