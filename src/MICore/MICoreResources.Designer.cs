1e0e5486fa6440bcea7c7bc67e0da514d5f2641d
Paul Maybee <paulmay@microsoft.com>
2016-05-23 11:07:17 -0700

    Fix build break in resource file generation

--- src/MICore/MICoreResources.Designer.cs
+++ src/MICore/MICoreResources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace MICore {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace MICore {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MICore.MICoreResources", typeof(MICoreResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MICore.MICoreResources", typeof(MICoreResources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
