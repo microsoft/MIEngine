fe54313d637539473d3fd325d4a9d1d778d6e055
Pierson Lee (PIE) <piel@microsoft.com>
2016-08-01 12:25:42 -0700

    Build Break fix

--- src/MIDebugEngine/ResourceStrings.Designer.cs
+++ src/MIDebugEngine/ResourceStrings.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Microsoft.MIDebugEngine {
     using System;
+	using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,7 @@ namespace Microsoft.MIDebugEngine {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.MIDebugEngine.ResourceStrings", typeof(ResourceStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.MIDebugEngine.ResourceStrings", typeof(ResourceStrings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
