488baf76364f8d8d0a37648fed304b99d4e360a7
Fangliang Xue <faxue@microsoft.com>
2016-08-17 13:36:54 -0700

    Fix enable breakpoint under run mode (#383)
    * Fix enable breakpoint under run mode
    
    Fix bug https://devdiv.visualstudio.com/DevDiv/VS%20Diag%20IntelliTrace/_workItems/index?_a=edit&id=226898&triage=true
    
    * Fix the start debugging with disabled breakpoint
    
    Fix bug https://devdiv.visualstudio.com/DefaultCollection/DevDiv/VS%20Diag%20IntelliTrace/_workItems?_a=edit&id=227613&triage=true

--- src/MIDebugEngine/AD7.Impl/AD7BoundBreakpoint.cs
+++ src/MIDebugEngine/AD7.Impl/AD7BoundBreakpoint.cs
@@ -20,10 +20,20 @@ namespace Microsoft.MIDebugEngine
         private AD7Engine _engine;
         private BoundBreakpoint _bp;
 
-        private bool _enabled;
         private bool _deleted;
 
-        internal bool Enabled { get { return _enabled; } }
+        internal bool Enabled
+        {
+            get
+            {
+                return _bp.Enabled;
+            }
+            set
+            {
+                _bp.Enabled = value;
+            }
+        }
+
         internal bool Deleted { get { return _deleted; } }
         internal ulong Addr
         {
@@ -38,7 +48,6 @@ namespace Microsoft.MIDebugEngine
             _engine = engine;
             _pendingBreakpoint = pendingBreakpoint;
             _breakpointResolution = breakpointResolution;
-            _enabled = true;
             _deleted = false;
             _bp = bp;
         }
@@ -65,7 +74,7 @@ namespace Microsoft.MIDebugEngine
         // Called by the debugger UI when the user is enabling or disabling a breakpoint.
         int IDebugBoundBreakpoint2.Enable(int fEnable)
         {
-            _enabled = fEnable == 0 ? false : true;
+            Enabled = fEnable == 0 ? false : true;
             return Constants.S_OK;
         }
 
@@ -92,11 +101,11 @@ namespace Microsoft.MIDebugEngine
             {
                 pState[0] = enum_BP_STATE.BPS_DELETED;
             }
-            else if (_enabled)
+            else if (Enabled)
             {
                 pState[0] = enum_BP_STATE.BPS_ENABLED;
             }
-            else if (!_enabled)
+            else if (!Enabled)
             {
                 pState[0] = enum_BP_STATE.BPS_DISABLED;
             }
