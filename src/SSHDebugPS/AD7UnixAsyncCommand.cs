dc9691fbf25b570f620590d209b24f48d5a3eea8
Rajkumar Janakiraman <rajkumar42@users.noreply.github.com>
2016-09-07 10:01:31 -0700

    When debugging through SSH as root, stdin is printed in stdout stream. (#434)
    * When debugging through SSH as root, stdin is printed in stdout stream.
    Fix: ignoring the stdin, if we see it in the stdout.

--- src/SSHDebugPS/AD7UnixAsyncCommand.cs
+++ src/SSHDebugPS/AD7UnixAsyncCommand.cs
@@ -23,6 +23,8 @@ namespace Microsoft.SSHDebugPS
         private bool _beginReceived;
         private bool _isClosed;
 
+        private string _startCommand;
+
         public AD7UnixAsyncCommand(IShellStream shellStream, IDebugUnixShellCommandCallback callback)
         {
             _shellStream = shellStream;
@@ -37,8 +39,8 @@ namespace Microsoft.SSHDebugPS
 
         internal void Start(string commandText)
         {
-            string fullCommand = string.Format("echo \"{0}\"; {1}; echo \"{2}$?\"", _beginMessage, commandText, _exitMessagePrefix);
-            _shellStream.WriteLine(fullCommand);
+            _startCommand = string.Format("echo \"{0}\"; {1}; echo \"{2}$?\"", _beginMessage, commandText, _exitMessagePrefix);
+            _shellStream.WriteLine(_startCommand);
             _shellStream.Flush();
         }
 
@@ -63,8 +65,6 @@ namespace Microsoft.SSHDebugPS
             //
             // Suggested fix: Add a ReadLine async method to the shell and remove the output received event
 
-            // TODO: rajkumar42, this breaks when logging in as root. 
-
             IEnumerable<string> linesToSend = null;
 
             lock (_lock)
@@ -81,6 +81,13 @@ namespace Microsoft.SSHDebugPS
                 if (_isClosed)
                 {
                     return;
+                }
+
+                if (line.Equals(_startCommand, StringComparison.Ordinal))
+                {
+                    // When logged in as root, shell sends a copy of stdin to stdout.
+                    // This ignores the shell command that was used to launch the debugger.
+                    continue;
                 }
 
                 int endCommandIndex = line.IndexOf(_exitMessagePrefix);
