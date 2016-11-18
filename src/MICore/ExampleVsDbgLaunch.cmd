0ace8b1ac5fab96204753fb1c63f668495d80d89
Wes Rupert <wesrupert@users.noreply.github.com>
2016-11-17 16:13:14 -0800

    Handle clrdbg to vsdbg rename (#501)

--- src/MICore/ExampleVsDbgLaunch.cmd
+++ src/MICore/ExampleVsDbgLaunch.cmd
@@ -1,17 +1,17 @@
 REM
-REM This is an example script for starting a project on Windows and attaching to it with clrdbg. 
+REM This is an example script for starting a project on Windows and attaching to it with vsdbg. 
 REM Various paths at the top of the script may need to be changed. 
 REM 
-REM See ExampleClrdbgLaunchOptions.Windows.xml for more information.
+REM See ExampleVsDbgLaunchOptions.Windows.xml for more information.
 REM 
 
 setlocal
-set clrdbg=C:\dd\VSPro_VBCS\binaries\amd64chk\Debugger\x-plat\Windows\clrdbg.exe
+set vsdbg=C:\dd\VSPro_VBCS\binaries\amd64chk\Debugger\x-plat\Windows\vsdbg.exe
 set ProjectName=AspNet5Con
 set ProjectDir=C:\proj\AspNet5Con\src\AspNet5Con
 set dnx=%USERPROFILE%\.dnx\runtimes\dnx-coreclr-win-x64.1.0.0-beta4-11296\bin\dnx.exe
 
-if not exist "%clrdbg%" echo ERROR: %clrdbg% does not exist.>&2 & exit /b -1
+if not exist "%vsdbg%" echo ERROR: %vsdbg% does not exist.>&2 & exit /b -1
 if not exist "%dnx%" echo ERROR: %dnx% does not exist.>&2 & exit /b -1
 
 start %dnx% --appbase %ProjectDir% "Microsoft.Framework.ApplicationHost"  --configuration Debug "%ProjectName%"
@@ -23,4 +23,4 @@ for /f "skip=3 tokens=2" %%p in ('tasklist /FI "IMAGENAME eq dnx.exe"') do set t
 
 if "%target_pid%"=="" echo ERROR: Failed to find dnx.exe process.>&2 & exit /b -1
 
-%clrdbg% --interpreter=mi --attach %target_pid%
+%vsdbg% --interpreter=mi --attach %target_pid%
