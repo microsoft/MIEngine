a0609237b6f16ce0e73769f99695b41c79971694
Rajkumar Janakiraman <janraj@microsoft.com>
2017-05-17 12:33:50 -0700

    Adding aka.ms link for more information.

--- scripts/GetClrDbg.sh
+++ scripts/GetClrDbg.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-echo "GetClrDbg.sh is retired. Use GetVsDbg.sh instead. If you are using Visual Studio, install Visual Studio 2017 Update 1 or newer."
+echo "GetClrDbg.sh is retired. Use GetVsDbg.sh instead. If you are using Visual Studio, install Visual Studio 2017 Update 1 or newer. See https://aka.ms/vsdbghelp for more details." 
 exit 1
