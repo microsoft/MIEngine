3bb8712bf0bf3843b173fc60b5540bb14ef5063b
Pierson Lee (PIE) <piel@microsoft.com>
2018-01-09 18:49:56 -0800

    Fixes to better support not supporting hitcounts

--- src/MIDebugEngine/AD7.Impl/AD7PendingBreakpoint.cs
+++ src/MIDebugEngine/AD7.Impl/AD7PendingBreakpoint.cs
@@ -385,7 +385,7 @@ namespace Microsoft.MIDebugEngine
                 // The breakpoint may not be bound for many reasons such as an invalid location, an invalid expression, etc...
                 // The sample engine does not support this, but a real world engine will want to return a valid enumeration of IDebugErrorBreakpoint2.
                 // The debugger will then display information about why the breakpoint did not bind to the user.
-                ppErrorEnum = null;
+                ppErrorEnum = new AD7ErrorBreakpointsEnum(new[] { this._BPError });
                 return Constants.S_FALSE;
             }
 
