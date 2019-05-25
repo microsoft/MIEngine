eb0ba6825935e60576cdfa0e1ff82fbcdf1242d5
Pierson Lee <piel@microsoft.com>
2019-05-24 18:52:46 -0700

    Code Review Feedback

---
+++ src/SSHDebugPS/SSH/SSHHelper.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using liblinux;
+using liblinux.Persistence;
+using Microsoft.SSHDebugPS.VS;
+using Microsoft.VisualStudio.Linux.ConnectionManager;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.SSHDebugPS.SSH
+{
+    public class SSHHelper
+    {
+        internal static SSHConnection CreateSSHConnectionFromConnectionInfo(ConnectionInfo connectionInfo)
+        {
+            if (connectionInfo != null)
+            {
+                UnixSystem remoteSystem = new UnixSystem();
+                string name = SSHPortSupplier.GetFormattedSSHConnectionName(connectionInfo);
+
+                while (true)
+                {
+                    try
+                    {
+                        VSOperationWaiter.Wait(string.Format(CultureInfo.CurrentCulture, StringResources.WaitingOp_Connecting, name), throwOnCancel: false, action: () =>
+                        {
+                            remoteSystem.Connect(connectionInfo);
+                        });
+                        break;
+                    }
+                    catch (RemoteAuthenticationException)
+                    {
+                        IVsConnectionManager connectionManager = (IVsConnectionManager)ServiceProvider.GlobalProvider.GetService(typeof(IVsConnectionManager));
+                        IConnectionManagerResult result = connectionManager.ShowDialog(StringResources.AuthenticationFailureHeader, StringResources.AuthenticationFailureDescription, connectionInfo);
+
+                        if ((result.DialogResult & ConnectionManagerDialogResult.Succeeded) == ConnectionManagerDialogResult.Succeeded)
+                        {
+                            connectionInfo = result.ConnectionInfo;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        VsShellUtilities.ShowMessageBox(ServiceProvider.GlobalProvider, ex.Message, null,
+                            OLEMSGICON.OLEMSGICON_CRITICAL, OLEMSGBUTTON.OLEMSGBUTTON_OK, OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                        return null;
+                    }
+                }
+
+                // NOTE: This will be null if connect is canceled
+                if (remoteSystem != null)
+                {
+                    return new SSHConnection(remoteSystem);
+                }
+            }
+
+            return null;
+        }
+
+        public static IEnumerable<ConnectionInfo> GetAvailableSSHConnectionInfos()
+        {
+            ConnectionInfoStore store = new ConnectionInfoStore();
+
+            return store.Connections.ToList().Select(item => (ConnectionInfo)item);
+        }
+    }
+}
