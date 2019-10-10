c2584e4167828ee953129a2483e0c1afa231bdba
Pierson Lee <piel@microsoft.com>
2019-10-09 21:58:54 -0700

    Fix accessibility issues
    Fix issues identified in accessibility testing for screenreading and
    high contrast

---
+++ src/SSHDebugPS/UI/Controls/Automation/ContainerListBoxAutomationPeer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Windows.Automation.Peers;
+
+namespace Microsoft.SSHDebugPS.UI
+{
+    public class ContainerListBoxAutomationPeer : ListBoxAutomationPeer
+    {
+        public ContainerListBoxAutomationPeer(ContainerListBox owner)
+            : base(owner)
+        { }
+
+        protected override ItemAutomationPeer CreateItemAutomationPeer(object item)
+        {
+            return new ContainerListBoxItemAutomationPeer<DockerContainerViewModel>(item, this);
+        }
+    }
+}
---
+++ src/SSHDebugPS/UI/Controls/Automation/ContainerListBoxItemAutomationPeer.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Windows.Automation;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+
+namespace Microsoft.SSHDebugPS.UI
+{
+    public class ContainerListBoxItemAutomationPeer<T> : ListBoxItemAutomationPeer, IExpandCollapseProvider where T : IContainerViewModel
+    {
+        public ContainerListBoxItemAutomationPeer(object item, ContainerListBoxAutomationPeer ownerAutomationPeer)
+            : base(item, ownerAutomationPeer)
+        {
+
+        }
+
+        /// <summary>
+        /// Item from the constructor is the viewmodel
+        /// </summary>
+        protected T ViewModel
+        {
+            get
+            {
+                return (T)Item;
+            }
+        }
+
+        #region IExpandCollapseProvider
+        public ExpandCollapseState ExpandCollapseState
+        {
+            get
+            {
+                if (ViewModel.IsExpanded)
+                {
+                    return ExpandCollapseState.Expanded;
+                }
+                else
+                    return ExpandCollapseState.Collapsed;
+            }
+        }
+
+        public void Expand()
+        {
+            ViewModel.IsExpanded = true;
+        }
+
+        public void Collapse()
+        {
+            ViewModel.IsExpanded = false;
+        }
+        #endregion
+        public override object GetPattern(PatternInterface patternInterface)
+        {
+            if (patternInterface == PatternInterface.ExpandCollapse)
+            {
+                return this;
+            }
+
+            return base.GetPattern(patternInterface);
+        }
+
+        #region Core overrides
+        protected override AutomationControlType GetAutomationControlTypeCore()
+        {
+            return AutomationControlType.Button;
+        }
+
+        protected override string GetLocalizedControlTypeCore()
+        {
+            return ControlType.Button.LocalizedControlType;
+        }
+
+        protected override string GetNameCore()
+        {
+            return ViewModel.ContainerAutomationName;
+        }
+
+        protected override bool IsKeyboardFocusableCore()
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
