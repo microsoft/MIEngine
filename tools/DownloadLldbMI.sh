935be528b97be21cdb4e1480871d2e751c39d348
Andrew Wang <waan@microsoft.com>
2021-07-20 12:39:02 -0700

    Address PR issues

---
+++ tools/DownloadLldbMI.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+lldbMIDownloadLink="https://download.visualstudio.microsoft.com/download/pr/173e6ced-0717-401d-87fc-169ca3424c72/f1228fd847c140b7f9839612f497bb7a/lldb-mi-10.0.0.zip"
+toolsDirectory=$(dirname "$0")
+
+# Go to root project folder
+pushd "$toolsDirectory/.." > /dev/null 2>&1 || exit 1
+
+if [ -z "$1" ] || [ "$1" == "-h" ]; then
+    echo "Please pass in output folder of publish"
+    echo "Example: $0 $(pwd)/bin/DebugAdapterProtocolTests/Debug/extension/debugAdapters"
+    exit 1
+fi
+
+if [ ! -f "./bin/DebugAdapterProtocolTests/Debug/extension/debugAdapters/OpenDebugAD7" ]; then
+    echo "Please build MIDebugEngine-Unix.sln and run PublishOpenDebugAD7.sh before running DownloadLldbMI.sh"
+    popd || exit
+    exit 1
+fi
+
+# Download the latest version of lldb-ui that vscode-cpptools uses.
+if ! `curl $lldbMIDownloadLink --output ./lldb-mi-10.0.0.zip > /dev/null 2>&1` ; then
+  echo "Failed to download lldb-mi."
+  exit 1
+fi
+
+unzip -o ./lldb-mi-10.0.0.zip > /dev/null 2>&1
+
+if [ ! -f ./debugAdapters/lldb-mi/bin/lldb-mi ]; then
+  echo "Failed to unzip."
+fi
+
+# Ensure we can run it or we will get permission denied.
+if ! `sudo chmod 755 ./debugAdapters/lldb-mi/bin/lldb-mi` ; then
+  echo "Failed to change permissions for lldb-mi."
+  exit 1
+fi
+
+# Clean up unused zip
+rm ./lldb-mi-10.0.0.zip
+
+popd > /dev/null 2>&1 || exit
