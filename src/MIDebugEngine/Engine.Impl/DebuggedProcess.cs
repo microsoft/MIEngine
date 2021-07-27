3d4577c41ff95bb3afaa98dd86c27adc22ceea04
Garrett Campbell <gcampbell@microsoft.com>
2021-07-27 14:36:13 -0400

    white space

--- src/MIDebugEngine/Engine.Impl/DebuggedProcess.cs
+++ src/MIDebugEngine/Engine.Impl/DebuggedProcess.cs
@@ -826,7 +826,6 @@ namespace Microsoft.MIDebugEngine
                         if (!string.IsNullOrWhiteSpace(destination))
                         {
                             commands.Add(new LaunchCommand("-target-select remote " + destination, string.Format(CultureInfo.CurrentCulture, ResourceStrings.ConnectingMessage, destination)));
-                            
                             if (localLaunchOptions.RequireHardwareBreakpoints && localLaunchOptions.HardwareBreakpointLimit > 0) {
                                 commands.Add(new LaunchCommand(string.Format(CultureInfo.InvariantCulture, "-interpreter-exec console \"set remote hardware-breakpoint-limit {0}\"", localLaunchOptions.HardwareBreakpointLimit.ToString(CultureInfo.InvariantCulture))));
                             }
