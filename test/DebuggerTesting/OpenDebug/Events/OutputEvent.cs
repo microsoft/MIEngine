8078acd0b9b11bcec56836176d70da9db886d2f9
Andrew Wang <waan@microsoft.com>
2021-08-27 17:32:20 -0700

    Fix LogPoint Issue
    Customer reported in v1.6.0 that LogPoints stopped working.
    This was due to IDebugBreakpointEvent2.EnumBreakpoints returning the
    same IEnumDebugBoundBreakpoints2.
    
    The fix was to save IDebugBoundBreakpoint2[] in the event and return a
    new IEnumDebugBoundBreakpoints2 object per call.
    
    Added a test for logpoints and fixed up ConsoleEvent to be OutputEvent
    since thats what it should actually be.

---
+++ test/DebuggerTesting/OpenDebug/Events/OutputEvent.cs
@@ -0,0 +1,43 @@
+﻿// // Copyright (c) Microsoft. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace DebuggerTesting.OpenDebug.Events
+{
+    public enum CategoryValue
+    {
+        Console = 0,
+        Stdout = 1,
+        Stderr = 2,
+        Telemetry = 3,
+        Unknown = Int32.MaxValue
+    }
+
+    public sealed class OutputEventValue : EventValue
+    {
+        public sealed class Body
+        {
+            public string output;
+
+            public string category;
+        }
+
+        public Body body = new Body();
+    }
+
+    public sealed class OutputEvent : Event<OutputEventValue>
+    {
+        public OutputEvent(string text, CategoryValue category) : base("output")
+        {
+            this.ExpectedResponse.body.category = GeCategory(category);
+            this.ExpectedResponse.body.output = text;
+        }
+
+        private static string GeCategory(CategoryValue category)
+        {
+            Parameter.ThrowIfIsInvalid(category, CategoryValue.Unknown, nameof(category));
+            return category.ToString().ToLowerInvariant();
+        }
+    }
+}
