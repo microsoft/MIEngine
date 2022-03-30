d47661d8894451d994b2450809911ac1b3369a6a
Andrew Wang <waan@microsoft.com>
2022-03-30 16:43:26 -0700

    Address PR issues

--- eng/Scripts/CI-Test.sh
+++ eng/Scripts/CI-Test.sh
@@ -20,28 +20,32 @@ if [ ! -f "$RootDir/bin/DebugAdapterProtocolTests/Debug/CppTests/config.xml" ]; 
 fi
 
 # Adapted from https://www.cazzulino.com/dotnet-test-retry.html
-counter=0
 exitcode=0
-while [ $counter -lt 6 ]
+dotnet test "$RootDir"/bin/DebugAdapterProtocolTests/Debug/CppTests/CppTests.dll --logger "trx;LogFileName=$RootDir/bin/DebugAdapterProtocolTests/Debug/CppTests/results.trx" | tee ./output.log
+# run test and forward output also to a file in addition to stdout (tee command)
+# capture dotnet test exit status, different from tee
+exitcode=${PIPESTATUS[0]}
+if [ "$exitcode" == 0 ]
+then
+    exit 0
+fi
+# Get failed test names, join as DisplayName=TEST with |, remove trailing |.
+filter=$(grep -o -P '(?<=\sFailed\s)\w*' < ./output.log | awk 'BEGIN { ORS="|" } { print("DisplayName=" $0) }' | grep -o -P '.*(?=\|$)')
+if [ -z "$filter" ] && [ "$(uname)" = "Darwin" ]; then
+    echo -e "Failed to set a filter. Make sure you are using GNU grep from homebrew."
+    exit 1
+fi
+
+# Make sure the next 3 runs are clean for the failing tests.
+counter=0
+while [ $counter -lt 3 ]
 do
-    if [ "$filter" ]
+    echo -e "Retry $counter for $filter"
+    dotnet test "$RootDir"/bin/DebugAdapterProtocolTests/Debug/CppTests/CppTests.dll --logger "trx;LogFileName=$RootDir/bin/DebugAdapterProtocolTests/Debug/CppTests/results-$counter.trx" --filter "$filter" > /dev/null 2>&1
+    if [ "$exitcode" != 0 ]
     then
-        echo -e "Retry $counter for $filter"
-        dotnet test "$RootDir"/bin/DebugAdapterProtocolTests/Debug/CppTests/CppTests.dll --logger "trx;LogFileName=$RootDir/bin/DebugAdapterProtocolTests/Debug/CppTests/results-$counter.trx" --filter "$filter" | tee ./output.log
-    else
-        dotnet test "$RootDir"/bin/DebugAdapterProtocolTests/Debug/CppTests/CppTests.dll --logger "trx;LogFileName=$RootDir/bin/DebugAdapterProtocolTests/Debug/CppTests/results.trx" | tee ./output.log
-    fi
-    # run test and forward output also to a file in addition to stdout (tee command)
-    # capture dotnet test exit status, different from tee
-    exitcode=${PIPESTATUS[0]}
-    if [ "$exitcode" == 0 ]
-    then
-        exit 0
-    fi
-    # cat output, get failed test names, join as DisplayName=TEST with |, remove trailing |.
-    filter=$(grep -o -P '(?<=\sFailed\s)\w*' < ./output.log | awk 'BEGIN { ORS="|" } { print("DisplayName=" $0) }' | grep -o -P '.*(?=\|$)')
-    if [ -z "$filter" ] && [ "$(uname)" = "Darwin" ]; then
-        echo -e "Failed to set a filter. Make sure you are using GNU grep from homebrew."
+        echo "Tests failed on rerun #$counter".
+        exit "$exitcode"
     fi
     ((counter++))
 done
