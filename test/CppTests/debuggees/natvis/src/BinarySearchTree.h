ef0efcb1744973346082e20660069a6536d761cf
Andrew Wang <waan@microsoft.com>
2022-06-02 18:22:01 -0700

    Add Tests for NatVis

---
+++ test/CppTests/debuggees/natvis/src/BinarySearchTree.h
@@ -0,0 +1,50 @@
+#include <cstddef>
+
+class BinarySearchTree
+{
+private:
+  class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+      data = value;
+    }
+  };
+
+  Node *root = NULL;
+  int numElements;
+
+public:
+  BinarySearchTree()
+  {
+    numElements = 0;
+  }
+
+  void Insert(int value)
+  {
+    root = InsertNode(root, value);
+    numElements++;
+  }
+
+  Node* InsertNode(Node *root, int value)
+  {
+    if (root == NULL)
+    {
+      return new Node(value);
+    }
+    else
+    {
+      if (root->data > value)
+      {
+        root->left = InsertNode(root->left, value);
+      }
+      else
+      {
+        root->right = InsertNode(root->right, value);
+      }
+    }
+  }
+};
